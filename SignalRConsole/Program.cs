957ffea1acf60aaa098b71c66ca35c68f7d6bd73
conqsolid <conqsolid@gmail.com>
2019-08-04 18:52:48 +0300

    initial

---
+++ SignalRConsole/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+
+namespace SignalRConsole
+{
+    class Program
+    {
+        static HubConnection connection;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            connection = new HubConnectionBuilder().WithUrl("http://localhost:5002/chathub").Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(1000);
+                Console.WriteLine($"Connection closed. Error: {error.ToString()}");
+                connect();
+            };
+
+            connection.On<string, string, string>("broadcastMessage", (user, message, id) =>
+            {
+                Console.WriteLine($"GetMessage:{ user}:{message}:{id}");
+            });
+
+            while (true)
+            {
+                var key = Console.Read();
+                if (key == 'c')
+                {
+                    connect();
+                }
+                else if (key == 'd')
+                {
+                    disconnect();
+                }
+
+            }
+        }
+
+        private static HubConnectionState disconnect()
+        {
+            try
+            {
+                connection.StopAsync().ContinueWith((t) =>
+                {
+                    Console.WriteLine($"Disconnect : Connection State: {connection.State}");
+
+                }
+                );
+                return connection.State;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Disconnect exception occured: {ex.ToString()}");
+                return HubConnectionState.Disconnected;
+            }
+        }
+
+        private static HubConnectionState connect()
+        {
+            try
+            {
+                connection.StartAsync().ContinueWith((t) =>
+                {
+                    Console.WriteLine($"Connect : Connection State: {connection.State}");
+                }
+                );
+                return connection.State;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connect exception occured: {ex.ToString()}");
+                return HubConnectionState.Disconnected;
+            }
+        }
+    }
+}
---
+++ SignalRWeb/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+
+        private readonly IHubContext<ChatHub> _hub;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _hub.Clients.All.SendAsync("broadcastMessage", "fatih", "selam", "123");
+            return new List<string> { "1", "2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
